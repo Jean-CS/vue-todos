d482bebf89d82c60f1d5102d10edac6709daf0e7
Jean Santos <jeansantosc@gmail.com>
2019-01-17 10:12:06 -0200

    adding Todos list component and rendering props

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
+
 export default {
   name: "app",
   components: {
-    
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false,
+        }
+      ]
+    }
   }
 };
 </script>
